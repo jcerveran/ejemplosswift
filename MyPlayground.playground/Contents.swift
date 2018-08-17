f10396dbe500515439fd7e32a482e48378d0aae1
jcerveran <jcerveran@gmail.com>
2018-08-17 12:56:18 +0200

    prueba de paso por valor y por referencia

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,17 @@
 
 import UIKit
 
-let constante = 10
-var variable = 20
+var a = 10
+var b = a
+b = 5
+a
 
-let cadenaconstante = "Hola Swift"
-var cadenavariable = "hello swift"
+let c = UILabel(frame: CGRect(x:0, y:0, width: 90, height: 100))
+c.text = "Etiqueta"
+c.text
 
-cadenavariable = "allo"
-
+let d = c
+d.text = "Label"
+d.text
+c.text
 
