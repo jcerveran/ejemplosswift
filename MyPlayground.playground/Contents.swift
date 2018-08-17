b0420b02e87f916b31938dc6adfa3e49924eb99a
jcerveran <jcerveran@gmail.com>
2018-08-17 12:01:33 +0200

    ejemplo boton

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,9 +2,12 @@
 
 import UIKit
 
-var contador = 0
+let testLabel = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 30)))
+testLabel.text = "¡Hola Swift!"
+testLabel.textAlignment = NSTextAlignment.center
+testLabel.backgroundColor = UIColor(red: 0.9, green: 0.0, blue:0.0, alpha: 1.0)
+testLabel.layer.cornerRadius = 5
+testLabel.layer.masksToBounds = true
 
-for i in 1...10 {
-    contador += i
-    print("contador : \(contador)")
-}
+testLabel
+
