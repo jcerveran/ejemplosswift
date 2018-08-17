f257ec2eeedb181347c6c65b3ea0d4f44bbb925d
jcerveran <jcerveran@gmail.com>
2018-08-17 11:35:37 +0200

    prueba print cadenas

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numero = 10
+var otro_numero = 20
+var cadena = "Swift"
+
+print (numero)
+print (otro_numero)
+print (numero, otro_numero, cadena)
+print (cadena)
+
+print("\(cadena) es un \(numero)")
+print("Le damos un \(numero) a \(cadena)")
