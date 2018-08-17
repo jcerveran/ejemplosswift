d55f54bef1a8470dac1dc262e9aeede9d0487cbf
jcerveran <jcerveran@gmail.com>
2018-08-17 12:49:47 +0200

    prueba de constantes y variables

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,12 @@
 
 import UIKit
 
-let testLabel = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 30)))
-testLabel.text = "¡Hola Swift!"
-testLabel.textAlignment = NSTextAlignment.center
-testLabel.backgroundColor = UIColor(red: 0.9, green: 0.0, blue:0.0, alpha: 1.0)
-testLabel.layer.cornerRadius = 5
-testLabel.layer.masksToBounds = true
+let constante = 10
+var variable = 20
 
-testLabel
+let cadenaconstante = "Hola Swift"
+var cadenavariable = "hello swift"
+
+cadenavariable = "allo"
+
 
