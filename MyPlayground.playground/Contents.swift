32221411db0649e9c09d9e1b83c4419a2d3e1c13
jcerveran <jcerveran@gmail.com>
2018-08-18 12:09:39 +0200

    operadores logicos y bucles

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,57 +2,59 @@
 
 import UIKit
 
-let a = 2
-let b = 3
-let c = 5
-let d = 0.6
-let e = 2.3
+var a = 10
+var b = 0
+do{
+    b = 20
+    print(a, b)
+}
+print(b)
 
-a + b
-b - a
-c * a
-a / b
-d / e
+let x = 2
 
-Double(a) / e
-a % b
+if (x < 2){
+    print("Es menor que 2")
+}
+else{
+    print("Es mayor o igual a 2")
+}
 
-var f = 2
-f += 2
-f -= 2
-f *= 2
-f /= 2
-f %= 2
+let y = 3
+let z = 4
+if x < 2{
+    print("Menor que 2")
+} else if y > 3{
+    print("Mayor que 3")
+} else if z==4{
+    print("Es 4")
+}
 
-let g = 2
-let h = -3
-let i = -g
-let j = -h
+let temperatura = 22
+if temperatura > 40{
+    print("¡Que calor!")
+}else if temperatura > 30{
+    print ("Empieza a hacer calor")
+}else if temperatura > 20{
+    print("Fresquito moderado")
+}else if temperatura > 10{
+    print("Hace frío moderado")
+}else {
+    print("Frío tal cual")
+}
 
-a == b
-a != b
-a <= b
-a >= b
-a < b
-a > b
+let vida = 6
+switch vida{
+case 5:
+    print("sano como una manzana")
+case 4:
+    print("vas bien pero cuidado")
+case 3, 2, 1:
+    print("estas pelechando")
+case 0:
+    print("estas muerto")
+default:()
+}
 
-let x = a == b
-
-let uilabelc = UILabel(frame: CGRect(x: 0, y:0, width: 90, height: 100))
-uilabelc.text = "Etiqueta"
-
-let duilabel = uilabelc
-duilabel.text = "Label"
-
-let y = duilabel === uilabelc
-
-let ta = 2
-let tb = -3
-
-let tc = 5
-let td = 6.5
-let te = 3
-
-ta == tb ? tc: te
-ta < tb ? "ta es menor" : "tb es menor"
-
+for indice in stride(from:1, to:20, by:3){
+    print("\n \(indice)")
+}
