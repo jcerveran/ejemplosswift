e2cc0402f9e61899c759959cb949853d66fe3b8b
jcerveran <jcerveran@gmail.com>
2018-08-18 12:14:52 +0200

    ejemplo de repeatwhile

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,60 +1,17 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-var a = 10
-var b = 0
-do{
-    b = 20
-    print(a, b)
-}
-print(b)
+var valor:Int
 
-let x = 2
+let acierto = Int(arc4random()) % 20
+print("Buscando el número \(acierto)")
 
-if (x < 2){
-    print("Es menor que 2")
-}
-else{
-    print("Es mayor o igual a 2")
-}
+repeat{
+    valor = Int(arc4random()) % 20
+    if acierto != valor{
+        print("No he acertado, ha salido el número \(valor)")
+    }
+}while acierto != valor
+print("Enhorabuena. Salió el \(valor)")
 
-let y = 3
-let z = 4
-if x < 2{
-    print("Menor que 2")
-} else if y > 3{
-    print("Mayor que 3")
-} else if z==4{
-    print("Es 4")
-}
-
-let temperatura = 22
-if temperatura > 40{
-    print("¡Que calor!")
-}else if temperatura > 30{
-    print ("Empieza a hacer calor")
-}else if temperatura > 20{
-    print("Fresquito moderado")
-}else if temperatura > 10{
-    print("Hace frío moderado")
-}else {
-    print("Frío tal cual")
-}
-
-let vida = 6
-switch vida{
-case 5:
-    print("sano como una manzana")
-case 4:
-    print("vas bien pero cuidado")
-case 3, 2, 1:
-    print("estas pelechando")
-case 0:
-    print("estas muerto")
-default:()
-}
-
-for indice in stride(from:1, to:20, by:3){
-    print("\n \(indice)")
-}
