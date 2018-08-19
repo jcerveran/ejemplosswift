947e6ea52f9b27a2790b6442a37732af343f822c
jcerveran <jcerveran@gmail.com>
2018-08-19 18:17:32 +0200

    cadenas

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,3 +6,19 @@ let cadena1 = "Una cadena"
 let cadena2 = "Otra cadena"
 let caracter1:Character = "A"
 let caracter2 = "B"
+
+let valor1 = 20.3
+let fecha = Date()
+let cadena = "El valor es \(valor1) y la fecha es \(fecha)"
+
+let valor2 = 10.1
+let cadenaSuma = "La suma es \(valor1 + valor2)"
+
+var inicio = "Érase una vez, en un pais lejano, un joven principe que vivía en un resplandeciente castillo"
+
+inicio.contains("lejano")
+inicio.range(of: "un ")
+
+if let rango = inicio.range(of: "pais"){
+    print(inicio.substring(with: rango))
+}
